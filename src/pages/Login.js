70c5e009a667a7fba4e26f66271949f46ecded0c
Fuad Mohammed Firoz <dev-fuad@outlook.com>
2017-11-11 21:50:43 +0530

    Setup login form

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
+import LoginForm from "../components/forms/LoginForm";
 
-const LoginPage = () => (
-  <h2>Login</h2>
-);
+class LoginPage extends Component {
+  state = {  }
+
+  submit = (data) => {
+    console.log(`Got data: ${data.email} & ${data.password}`);
+  }
+
+  render() {
+    return (
+      <div>
+      <h2>Login</h2>
+      <LoginForm submit={this.submit} />
+    </div>
+    );
+  }
+}
 
 export default LoginPage;
