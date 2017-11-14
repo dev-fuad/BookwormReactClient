9a67dfc8d4be207cacab38513e4bc4858f37fed1
Fuad Mohammed Firoz <dev-fuad@outlook.com>
2017-11-15 00:39:15 +0530

    Bug Fix: Users token is not saved in localStorage upon registration.

--- src/store/actions/register.js
+++ src/store/actions/register.js
@@ -2,4 +2,7 @@ import api from "../../providers/api";
 import { userLoggedIn } from "./auth";
 
 export const signup = data => dispatch =>
-  api.user.signup(data).then(user => dispatch(userLoggedIn(user)));
+  api.user.signup(data).then(user => {
+    localStorage.bookWormJWT = user.token;
+    dispatch(userLoggedIn(user))
+  });
