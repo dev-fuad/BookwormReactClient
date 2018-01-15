51eba7398225c135f72f0534b38bae4f7913dce6
Fuad Mohammed Firoz <dev-fuad@outlook.com>
2018-01-16 00:57:37 +0530

    Replaced Gravatar with Robohash

--- src/components/navigation/TopNavigation.js
+++ src/components/navigation/TopNavigation.js
@@ -3,7 +3,6 @@ import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import { Menu, Dropdown, Image } from "semantic-ui-react";
-import gravatar from "gravatar-url";
 import * as actions from "../../store/actions/auth";
 import { allBooksSelector } from "../../store/reducers/books";
 
@@ -18,7 +17,7 @@ const TopNavigation = ({ user, logout, hasBooks }) => (
       </Menu.Item>
     )}
     <Menu.Menu position="right">
-      <Dropdown trigger={<Image avatar src={gravatar(user.email)} />}>
+      <Dropdown trigger={<Image avatar src={`https://robohash.org/set_set3/${user.email}?size=50x50`} />}>
         <Dropdown.Menu>
           <Dropdown.Item onClick={() => logout()}>Logout</Dropdown.Item>
         </Dropdown.Menu>
